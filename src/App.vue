376ee3f30cd6f27c490c3c3ca5aa629ee32d945d
juhan <johann.junti@tptlive.ee>
2024-02-20 13:00:37 +0200

    Vue app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <script setup>
+import { ref } from 'vue';
 
+
+let message = ref('Helou Vue'); 
+let items = ref(['Sai', 'muna', 'piim', 'või'])
+function add(){
+  if(message.value.trim() !== ''){
+    items.value.push(message.value.trim())
+  }
+  message.value = '';
+}
 </script>
 
 <template>
+<button @click="add">Click me</button>
+<input type="text" v-model="message" @keydown.enter="add">
+<ul>
+  <li v-for="item in items">{{ item }}</li>
+</ul>
 </template>
