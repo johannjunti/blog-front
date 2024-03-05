f0ed291c723480b0dd3ba1e5a696569df4bbe598
juhan <johann.junti@tptlive.ee>
2024-03-05 13:21:38 +0200

    Router, Nav

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import './style.css';
+import Buefy from '@ntohq/buefy-next';
+import '@ntohq/buefy-next/dist/buefy.css';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+
+import Home from './pages/Home.vue';
+import ToDo from './pages/Todo.vue';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/todo', component: ToDo },
+];
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We
+    // are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+})
+  
+const app = createApp(App);
+app.use(router);
+app.use(Buefy);
+app.mount('#app');
---
+++ src/pages/Todo.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, computed } from 'vue';
+import ItemList from '../component/ItemList.vue';
+
+let i = 0;
+let message = ref('Helou Vue'); 
+let items = ref([
+  {id: i++, name:'Sai', isDone: true},
+  {id: i++, name:'Muna', isDone: false},
+  {id: i++, name:'Piim', isDone: true},
+  {id: i++, name:'Või', isDone: false},
+]);
+
+function add(){
+  if(message.value.trim() !== ''){
+    items.value.push({id: i++, name:message.value.trim(), isDone: false});
+  }
+  message.value = '';
+}
+
+let doneItems = computed(() => {
+  return items.value.filter(item => item.isDone)
+});
+let todoItems = computed(() => {
+  return items.value.filter(item => !item.isDone)
+});
+</script>
+
+<template>
+<button @click="add">Click me</button>
+<input type="text" v-model="message" @keydown.enter="add">
+
+<ItemList :items="items" title="All Items"></ItemList>
+<ItemList :items="doneItems" title="Done Items"></ItemList>
+<ItemList :items="todoItems" title="Todo Items"></ItemList>
+
+</template>
